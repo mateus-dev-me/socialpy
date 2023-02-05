5e128837b3c9bfe4f05b7ac5b7bb22550bc4dfd7
mateus-dev-me <mateusmartinsipu2@gmail.com>
2023-02-05 02:31:42 -0300

    [feat]: Incializando configurações do dynaconf

---
+++ socialpy/config.py
@@ -0,0 +1,10 @@
+from dynaconf import Dynaconf
+
+
+settings = Dynaconf(
+    envvar_prefix='socialpy',
+    settings_files=['settings.toml', '.secrets.toml'],
+    environments=['development', 'production', 'testing'],
+    env_switcher='socialpy_env',
+    load_dotenv=False,
+)
