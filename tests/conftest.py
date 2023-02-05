422753c5411e6569d659493fb4529a55a229635c
mateus-dev-me <mateusmartinsipu2@gmail.com>
2023-02-05 02:39:34 -0300

    [test]: Adicionando test da api

---
+++ tests/conftest.py
@@ -0,0 +1,43 @@
+import os
+
+import pytest
+from fastapi.testclient import TestClient
+from socialpy.app import app
+from socialpy.cli import create_user
+from sqlalchemy.exc import IntegrityError
+
+os.environ[
+    'socialpy_DB__uri'
+] = 'postgresql://postgres:postgres@db:5432/socialpy_test'
+
+
+@pytest.fixture(scope='function')
+def api_client():
+    return TestClient(app)
+
+
+def create_api_client_authenticated(username):
+
+    try:
+        create_user(f'{username}@socialpy.com', username, username)
+    except IntegrityError:
+        pass
+
+    client = TestClient(app)
+    token = client.post(
+        '/token',
+        data={'username': username, 'password': username},
+        headers={'Content-Type': 'application/x-www-form-urlencoded'},
+    ).json()['access_token']
+    client.headers['Authorization'] = f'Bearer {token}'
+    return client
+
+
+@pytest.fixture(scope='function')
+def api_client_user1():
+    return create_api_client_authenticated('user1')
+
+
+@pytest.fixture(scope='function')
+def api_client_user2():
+    return create_api_client_authenticated('user2')
