e3e0ebcd191cd523ffd2950a0ce5498d7f4980c3
mateus-dev-me <mateusmartinsipu2@gmail.com>
2023-02-10 21:06:51 -0300

    [refactor]: Refatorando funções de manipulação de token

---
+++ socialpy/utils/auth.py
@@ -0,0 +1,116 @@
+from datetime import datetime, timedelta
+from typing import Callable, Optional, Union
+
+from fastapi import Depends, HTTPException, Request, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from sqlmodel import Session, select
+
+from socialpy.config import settings
+from socialpy.db.db import engine
+from socialpy.db.models.user import User
+from socialpy.db.schemas import *
+from socialpy.utils.security import verify_password
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+
+SECRET_KEY = settings.security.secret_key
+ALGORITHM = settings.security.algorithm
+
+
+def create_access_token(
+    data: dict, expires_delta: Optional[timedelta] = None
+) -> str:
+    """Creates a JWT Token from user data"""
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({'exp': expire, 'scope': 'access_token'})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def create_refresh_token(
+    data: dict, expires_delta: Optional[timedelta] = None
+) -> str:
+    """Refresh an expired token"""
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({'exp': expire, 'scope': 'refresh_token'})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
+def authenticate_user(
+    get_user: Callable, username: str, password: str
+) -> Union[User, bool]:
+    """Authenticate the user"""
+    user = get_user(username)
+    if not user:
+        return False
+    if not verify_password(password, user.password):
+        return False
+    return user
+
+
+def get_user(username) -> Optional[User]:
+    """Get user from database"""
+    query = select(User).where(User.username == username)
+    with Session(engine) as session:
+        return session.exec(query).first()
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme), request: Request = None, fresh=False
+) -> User:
+    """Get current user authenticated"""
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail='Could not validate credentials',
+        headers={'WWW-Authenticate': 'Bearer'},
+    )
+
+    if request:
+        if authorization := request.headers.get('authorization'):
+            try:
+                token = authorization.split(' ')[1]
+            except IndexError:
+                raise credentials_exception
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = get_user(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    if fresh and (not payload['fresh'] and not user.superuser):
+        raise credentials_exception
+
+    return user
+
+
+async def get_current_active_user(
+    current_user: User = Depends(get_current_user),
+) -> User:
+    """Wraps the sync get_active_user for sync calls"""
+    return current_user
+
+
+AuthenticatedUser = Depends(get_current_active_user)
+
+
+async def validate_token(token: str = Depends(oauth2_scheme)) -> User:
+    """Validates user token"""
+    user = get_current_user(token=token)
+    return user
