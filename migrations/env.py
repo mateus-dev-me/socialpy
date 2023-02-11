c77128ea948a61e7387dcb50634f90f482fe1278
mateus-dev-me <mateusmartinsipu2@gmail.com>
2023-02-10 21:02:58 -0300

    [add]: migrations

--- migrations/env.py
+++ migrations/env.py
@@ -1,10 +1,11 @@
 from logging.config import fileConfig
 
 from alembic import context
+from sqlalchemy import engine_from_config, pool
+
 from socialpy import models
 from socialpy.config import settings
 from socialpy.db import engine
-from sqlalchemy import engine_from_config, pool
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
